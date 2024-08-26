13fc1c3390960a333abb85a37bfcd1df4425cac4
luizaki <francinelouise.sanchez@gmail.com>
2024-08-26 17:01:27 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# analog-clock
+A simple analog clock made with HTML, CSS, and JavaScript. In partial requirements for Modeling and Simulation course.
+
+## technologies
+<p align="left">
+<img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+<img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=333" />
+</p>
+
+## references
+[Bui, H. (2023, July 3). Creating an Analog Clock with HTML, CSS, and JavaScript. Medium.](https://medium.com/@williamhuybui/creating-an-analog-clock-with-html-css-and-javascript-32a254da8a9e)
+[Code Braces. (2023). Analog Clock HTML CSS Javascript Dark Mode. In YouTube.](https://www.youtube.com/watch?v=NlAw_kd2608)
---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+// retrieve hands
+const secondsHand = document.getElementById('second-hand');
+const minutesHand = document.getElementById('minute-hand');
+const hoursHand = document.getElementById('hour-hand');
+
+// retrieve calendar window
+const calendar = document.getElementById('calendar-window');
+
+// function to retrieve current time and call rotate hand function accordingly
+function updateClock() {
+    const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+    const current = new Date();
+
+    // get respective time components and proportionize them
+    let seconds = current.getSeconds() / 60;
+    let minutes = (seconds + current.getMinutes()) / 60;
+    let hours = (minutes + current.getHours()) / 12;
+
+    // update hand rotation
+    rotateHand(secondsHand, seconds);
+    rotateHand(minutesHand, minutes);
+    rotateHand(hoursHand, hours);
+
+    // update calendar window
+    let month = months[current.getMonth()];
+    let date = current.getDate().toString().padStart(2, '0');
+
+    calendar.innerHTML = `${month} ${date}`;
+}
+
+// function to rotate the hand of each component
+function rotateHand(hand, rotation) {
+    hand.style.setProperty('--rotation', rotation * 360);
+}
+
+// calling the function every second
+setInterval(updateClock, 1000);
